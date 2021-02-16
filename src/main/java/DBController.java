cbc1bf07a143a261a7f666c5b277b8c945e3c0b6
Saabendtsen <saabendtsen@gmail.com>
2021-02-16 09:39:36 +0100

    DB basic command Commit

---
+++ src/main/java/DBController.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBController {
+
+
+}
