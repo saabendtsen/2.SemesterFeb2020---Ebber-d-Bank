a4b87d2f85f3db42153bc5617ba6d89cf77a21b8
Saabendtsen <saabendtsen@gmail.com>
2021-02-16 11:38:01 +0100

    handler done

--- src/main/java/TransaktionHandler.java
+++ src/main/java/TransaktionHandler.java
@@ -6,32 +6,28 @@ public class TransaktionHandler {
 
 
     public void deposit(Account account, double amount){
-
-
-        //Transaction t = new Transaction(account.getAccount_id(), amount);
-
-
-        account.updateAccountAmount();
-
+        if (amount > 0) {
+            dbc.depositFromAccount(account, amount);
+        }
     }
 
     public void withdraw(Account account, double amount){
-        account.updateAccountAmount();
 
         if(account.getCurrentAmount() > amount){
         dbc.withdrawFromAccount(account,amount);
         } else {
             System.out.println("Der er ikke nok penge på din konto. Dit fattige svin");
         }
-
-
     }
 
-    public void transferBetweenAccount(Account fromAccount, Account toAccount, double Amount){
-
+    public void transferBetweenAccount(Account fromAccount, Account toAccount, double amount){
+        if(fromAccount.getCurrentAmount() > amount) {
+            withdraw(fromAccount, amount);
+            deposit(toAccount, amount);
+        }
     }
 
     public void showAllTransaktions(Account account){
-
+        dbc.showAllTransactions(account);
     }
 }
