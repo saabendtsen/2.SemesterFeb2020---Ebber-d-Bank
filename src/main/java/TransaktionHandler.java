1428bd7044f5c8cac9a8d2feb98a393659027a38
Saabendtsen <saabendtsen@gmail.com>
2021-02-16 10:04:20 +0100

    commit Transaktionhandler

---
+++ src/main/java/TransaktionHandler.java
@@ -0,0 +1,20 @@
+public class TransaktionHandler {
+
+
+
+    public void deposit(Account account, double amount){
+    }
+
+    public void withdraw(Account account, double amount){
+
+    }
+
+    public void transferBetweenAccount(Account fromAccount, Account toAccount, double Amount){
+
+    }
+
+
+    public void showAllTransaktions(Account account){
+
+    }
+}
