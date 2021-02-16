62789f9026ef3bec0de4e1d382aa9053e626d992
August Manniche <august.manniche@gmail.com>
2021-02-16 09:40:10 +0100

    All the class is filled out

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,36 @@
 public class Bank {
+
+    private String name;
+    private String Bank_id;
+    private String City;
+
+    public Bank(String name, String bank_id, String city) {
+        this.name = name;
+        Bank_id = bank_id;
+        City = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBank_id() {
+        return Bank_id;
+    }
+
+    public void setBank_id(String bank_id) {
+        Bank_id = bank_id;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
 }
--- src/main/java/Costumer.java
+++ src/main/java/Costumer.java
@@ -1,2 +1,36 @@
 public class Costumer {
+
+    private String costumer_id;
+    private String name;
+    private String city;
+
+    public Costumer(String costumer_id, String name, String city) {
+        costumer_id = costumer_id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getCostumer_id() {
+        return costumer_id;
+    }
+
+    public void setCostumer_id(String costumer_id) {
+        this.costumer_id = costumer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
