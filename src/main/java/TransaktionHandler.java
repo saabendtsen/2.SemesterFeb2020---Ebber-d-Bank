d4ab4c90d3cb80ff279a218c2726308a856bbd07
Saabendtsen <saabendtsen@gmail.com>
2021-02-16 10:24:04 +0100

    halvassed class methods

--- src/main/java/TransaktionHandler.java
+++ src/main/java/TransaktionHandler.java
@@ -1,18 +1,34 @@
 public class TransaktionHandler {
 
+    DBController db = new DBController();
+
 
 
     public void deposit(Account account, double amount){
+
+
+        account.updateAccountAmount();
+
+
+
     }
 
     public void withdraw(Account account, double amount){
+        account.updateAccountAmount();
+
+        if(account.getCurrentAmount() > amount){
+
+
+        } else {
+            System.out.println("Der er ikke nok penge på din konto. Dit fattige svin");
+        }
+
 
     }
 
     public void transferBetweenAccount(Account fromAccount, Account toAccount, double Amount){
 
     }
-
 
     public void showAllTransaktions(Account account){
 
