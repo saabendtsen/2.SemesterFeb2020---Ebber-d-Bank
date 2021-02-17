9d13769834c6112b826e9c5703f9498bc24cc287
August Manniche <august.manniche@gmail.com>
2021-02-17 21:36:06 +0100

    Menu class fix

--- src/main/java/TransaktionHandler.java
+++ src/main/java/TransaktionHandler.java
@@ -6,8 +6,10 @@ public class TransaktionHandler {
 
 
     public void deposit(int account_id, double amount){
+
         if (amount > 0) {
             dbc.transferFromToAccount(account_id, amount);
+            System.out.println("Du har nu deposit: " + amount + "kr" + " på konto nummer: " + account_id + "\n");
         }
     }
 
@@ -15,16 +17,15 @@ public class TransaktionHandler {
 
         if(dbc.returnCurrentAccountAmount(account_id) > amount){
         dbc.transferFromToAccount(account_id,-amount);
+            System.out.println("Du har nu hævet: " + amount + "kr" + " fra konto nummer: " + account_id);
         } else {
             System.out.println("Der er ikke nok penge på din konto. Dit fattige svin");
         }
     }
 
     public void transferBetweenAccount(int fromAccount, int toAccount, double amount){
-        if(fromAccount > amount) {
             withdraw(fromAccount, amount);
             deposit(toAccount, amount);
-        }
     }
 
     public void showAllTransactions(Account account){
