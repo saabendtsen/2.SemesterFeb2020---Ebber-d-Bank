1c959f913597bed6ec6a6fce8de211c757031b89
August Manniche <august.manniche@gmail.com>
2021-02-18 09:09:51 +0100

    Changes to menu class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     // Database info
     static final String DBURL = "jdbc:mysql://localhost:3306/bank?serverTimezone=UTC";
     static final String DBUSER = "root";
-    static final String DBPASS = "1234";
+    static final String DBPASS = "xag32zrx";
 
     public static void main(String[] args) {
         Menu m = new Menu();
