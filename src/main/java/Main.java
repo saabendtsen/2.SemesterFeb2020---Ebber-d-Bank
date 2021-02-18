50d5ee329cb81fbc1f79bca888d7856ba6986fbe
August Manniche <august.manniche@gmail.com>
2021-02-18 10:18:05 +0100

    Hej

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     // Database info
     static final String DBURL = "jdbc:mysql://localhost:3306/bank?serverTimezone=UTC";
     static final String DBUSER = "root";
-    static final String DBPASS = "xag32zrx";
+    static final String DBPASS = "";
 
     public static void main(String[] args) {
         Menu m = new Menu();
