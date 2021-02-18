45127f7998edea36046b4adcfc0a864890ec8bb2
August Manniche <august.manniche@gmail.com>
2021-02-18 09:34:57 +0100

    Clean up in code and deletes some unused classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,47 +13,4 @@ public class Main {
     }
 
 
-    // Deposit på account
-        /*
-        boolean result = DBController.depositFromAccount(account,amount);
-            if (result) {
-                System.out.println("Indsat "+amount+" på konto "+ account);
-            } else {
-                System.out.println(account+" Findes ikke!");
-            }
-         */
-
-    // Delete account
-        /*
-        boolean result = DBController.deleteCustomer(accountID);
-            if (result) {
-                System.out.println("konto:  "+accountID+" er nu slettet");
-            } else {
-                System.out.println(accountID+" Findes ikke!");
-            }
-         */
-
-    // insert new customer
-        /*
-        public void adduser(){
-            System.out.println("---Indsæt ny kunde---");
-            System.out.println("indtast kunder navn");
-            String customer_name = sc.nextLine();
-            System.out.println("indtast kundens by");
-            String customer_city = sc.nextLine();
-            Customer newCustomer = new Customer(0,customer_name,customer_city);
-            boolean result = dbc.createCustomer(newCustomer);
-            if (result){
-                System.out.println("Kunde med nummer "+" er nu blevet tilføjet til DB");
-            } else {
-                System.out.println("Kunde kunne ikke tilføjes! Kunde ID "+" bruges allerede!");
-            }
-        }
-         */
-
-    // see all transaktions on account id
-    /*
-    System.out.println(showAllTransactions(account id));
-     */
-
 }
